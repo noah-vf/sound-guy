503e67af5d11d456fcbb66cbf64c78e0217f0420
noah <4176662+noah-vf@users.noreply.github.com>
2023-06-01 23:55:02 -0400

    bringing noninterpolated code to v2

--- Code/sound-guy-attiny-noninterpolated.ino
+++ Code/sound-guy-attiny-noninterpolated.ino
@@ -1,23 +1,13 @@
 /* THIS LIBRARY DOES NOT SUPPORT INTERPOLATION. 1 Band is repeated in 2 of the 8 rows.
-     Shift Register Audio Display, v1 
+     Shift Register Audio Display, v2
      Uses shiftRegDisplay and MSGEQ7 classes to generate a graphic EQ 
+     This version is rotated right for hardware version 2
      To do: AGC?
 */
-#include "MSGEQ7.h"
-#include "shiftRegDisplay.h"
 
-// Pinmap for Arduino Uno. Uncomment to Enable
-/*
-#define pinReset A1
-#define pinStrobe A2
-#define pinAnalog A3
-#define pinLatch 8
-#define pinColClock 9
-#define pinColData 10
-#define pinRowClock 12
-#define pinRowData 13
-*/
-// Pinmap for ATTiny84. Uncomment to enable.
+#include "MSGEQ7.h"
+#include "shiftRegDisplayATT.h"
+
 
 #define pinReset 7
 #define pinStrobe 1
@@ -29,22 +19,20 @@
 #define pinRowData 10
 
 
+#define MSGEQ7_INTERVAL ReadsPerSecond(60)
+#define MSGEQ7_SMOOTH 100// Range: 0-255
 
-
-#define MSGEQ7_INTERVAL ReadsPerSecond(50)
-#define MSGEQ7_SMOOTH 100 // Range: 0-255
-
-byte averages[8][20];
-shiftRegDisplay disp(pinColClock,pinColData,pinRowClock,pinRowData,pinLatch,8000);
+shiftRegDisplayATT disp(pinColClock,pinColData,pinRowClock,pinRowData,pinLatch,80000);
 CMSGEQ7<MSGEQ7_SMOOTH, pinReset, pinStrobe, pinAnalog> MSGEQ7;
 
 
 
 void setup() {
   MSGEQ7.begin();
+
 }
 
-byte valueMap(byte inputValue) { //Map analog readings to a number that corresponds to all the LEDs in a certain column
+byte valueMap(byte inputValue) { //Map analog readings to a number that corresponds to all the LEDs in a certain column. Byte -> "Row of LEDS".
   if (inputValue<32){
     return(1);
   }
@@ -71,14 +59,13 @@ byte valueMap(byte inputValue) { //Map analog readings to a number that correspo
   }
 }
 
-int arrayAverage(byte inputArray[20]) {   
-  int average = (inputArray[0] + inputArray[1]+ inputArray[2]+ inputArray[3]+ inputArray[4]+ inputArray[5]+ inputArray[6]+ inputArray[7]+ inputArray[8]+ inputArray[9]+ inputArray[10]+ inputArray[11]+ inputArray[12]+ inputArray[13]+ inputArray[14]+ inputArray[15]+ inputArray[16]+ inputArray[17]+ inputArray[18]+ inputArray[19])/20;
-  return(average);
-}
+
 
 void loop() {   //If a new reading is available, get it, map it, push it to display. If not, keep updating display.
   bool newReading = MSGEQ7.read(MSGEQ7_INTERVAL);
-  if (newReading) {
+  byte calcValues[8];
+
+  if (newReading) {               // Take new measurement 
    byte bass = MSGEQ7.get(MSGEQ7_0);
     bass = mapNoise(bass);
     byte bass2 = MSGEQ7.get(MSGEQ7_1);
@@ -94,21 +81,28 @@ void loop() {   //If a new reading is available, get it, map it, push it to disp
     byte high = MSGEQ7.get(MSGEQ7_6);
     high = mapNoise(high);
 
-    int  scaleFactor = 1;
-    
-    disp.colValues[0]= byte(valueMap(scaleFactor*bass));
-    disp.colValues[1]= byte(valueMap(scaleFactor*bass2));
-    disp.colValues[2]= byte(valueMap(scaleFactor*low));
-    disp.colValues[3]= byte(valueMap(scaleFactor*low2));
-    disp.colValues[4]= byte(valueMap(scaleFactor*mid));
-    disp.colValues[5]= byte(valueMap(scaleFactor*mid2));
-    disp.colValues[6]= byte(valueMap(scaleFactor*mid2));
-    disp.colValues[7]= byte(valueMap(scaleFactor*high));
-
-
-    
-    
+    calcValues[7] = bass;	
+    calcValues[6] = bass;							
+    calcValues[5] = bass2;
+    calcValues[4] = low;
+    calcValues[3] = low2;
+    calcValues[2] = mid;
+    calcValues[1] = mid2;
+    calcValues[0] = high;
+  
 
    }
+
+   
+  for (int y = 0; y < 8; y++){                      //Rotate the display right. For every new column value, derive it from unrotated column values
+    byte newValue = 0; 
+    for (int j = 0; j<8; j++){                      
+      if ((calcValues[7-j] & (1<<y)) > 0) {
+        newValue = newValue + (1<<j);
+      }
+    }
+    disp.colValues[y] = newValue;               
+  }
+
  disp.updateDisplay();
 }
